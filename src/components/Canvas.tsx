120c3d15d9ec8a4ccd9e3b55a4a4220e1a4419ba
Mackay <mackay@Mackay-Mac-mini.local>
2021-04-26 15:11:21 +0900

    chnage canvas ref functional to callback

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -5,13 +5,5 @@ interface Props {
 }
 
 export const Canvas: FC<Props> = memo(({ initApp }) => {
-  const ref = useRef<HTMLCanvasElement>(null);
-
-  useEffect(() => {
-    if (!ref.current) return;
-
-    initApp(ref.current);
-  }, [ref]);
-
-  return <canvas ref={ref}>Canvs viewer</canvas>;
+  return <canvas ref={initApp}>Canvs viewer</canvas>;
 });
